3cfa72a91229bcda1650006dbefe03e9167c1dda
phil <uniphil@gmail.com>
2014-04-22 15:48:41 -0400

    Put the map on the home page
    There are no tile layers at the moment, but leaflet loads and the
    controls show up.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+
+// draw a map
+(function drawMap(mapEl) {
+  if (!mapEl) {
+    return;
+  }
+  var kiberaCentre = [-1.315, 36.785];
+  var map = L.map('map').setView(kiberaCentre, 13);
+})(document.getElementById('map'));
+
