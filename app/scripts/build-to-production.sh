47322e795c99ebdc20611713a80ff42138b80377
Phil Schleihauf <uniphil@gmail.com>
2014-05-21 03:14:24 -0400

    kinda sorta work'

--- app/scripts/build-to-production.sh
+++ app/scripts/build-to-production.sh
@@ -9,34 +9,37 @@
 set -o nounset
 set -o errexit
 
+# get into gh-pages to start off
+echo "checking out gh-pages..."
+git fetch -v
+git checkout gh-pages
+git pull
+
 # try to set up a temporary local folder for a repo we can work in without risking screwing up our actual local repo
-mkdir local-production-repo || true  # it might already exist
+echo "creating local working repo..."
+mkdir local-production-repo
 cd local-production-repo
 git clone .. .
-git fetch --all
+git fetch -v
 
 # go the the gh-pages branch and clean up
-git checkout gh-pages
 git rm -r *
 
 # copy in the newly-built site
-cp ../build/* .
+cp -r ../build/* .
 
 # commit!
-git add .
+git add --all .
 git commit -m "auto-commit production branch"
 
 # add the tmp local to the real local
 cd ..
 git remote add localproduction local-production-repo
-git fetch -v local-production-repo
-
-# checkout the stuff
-git checkout gh-pages
+git pull localproduction gh-pages
 
 
 echo "Ready to push."
 echo "You might want to test it one last time (python2 -m SimpleHTTPServer)."
-echo "When ready, simply `git push origin gh-pages`."
+echo "When ready, simply \`git push origin gh-pages\`."
 echo "You are currently on the gh-pages branch."
-echo "`git checkout master` will bring you back to the source files."
+echo "\`git checkout master\` will bring you back to the source files."
