9c66859aa7401513556dab25efd2dfb6e5793ee4
Phil Schleihauf <uniphil@gmail.com>
2014-05-23 09:07:37 -0400

    remove old dead code

--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,4 @@
     ~~~
 
     Static site generator for the Kibera School Project by Feedback Labs
-
-    This file initializes the app and manages configuration before importing
-    the other modules to do all the work.
-
 """
-
-# from flask import Flask
-
-# app = Flask('app')
-
-# app.config['FREEZER_DESTINATION'] = app.config['BUILD_OUTPUT'] = '../output'
-# app.config['CONTENT_FOLDER'] = '../content'
-# app.config['CNAME'] = 'kibera-staging.precomp.ca'
-
-# from . import content
-# from . import views
-# from . import static
