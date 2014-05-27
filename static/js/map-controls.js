ddf24255cedb6e8cc3f94367c95fdda1cd743c74
phil <uniphil@gmail.com>
2014-05-27 14:12:53 -0400

    fix up the browse overlay and quicksearch

--- static/js/map-controls.js
+++ static/js/map-controls.js
@@ -17,15 +17,23 @@
 
   // quick search functionality for schools browse
   (function schoolQuickSearch(listNode) {
+
+    function simplify(str) {
+      lowered = str.toLowerCase();
+      unspecialed = lowered.replace(/[^a-z]/g, '');
+      return unspecialed;
+    }
+
     var nameNodes = listNode.children;
     var namesMap = u.mapNodes(nameNodes, function mapNames(node) {
-      var name = node.textContent.toLowerCase();
+      var name = simplify(node.textContent || el.innerText);
       return {'name': name,
               'node': node}
     });
+    console.log(namesMap);
     var searchInput = controlsContainer.querySelector('.school-list > input');
     u.on(searchInput, 'keyup', function liveFilterSchools(evt) {
-      var startsWith = searchInput.value.toLowerCase();
+      var startsWith = simplify(searchInput.value);
       namesMap.forEach(function filterSchool(school) {
         if (u.startsWith(school.name, startsWith)) {
           u.removeClass(school.node, 'hidden');
