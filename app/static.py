13f38301b42d0621e835112a569d1db98e54969f
phil <uniphil@gmail.com>
2014-05-30 17:56:03 -0400

    fix random static compiling bug

--- app/static.py
+++ app/static.py
@@ -38,17 +38,17 @@ def cssmin(sources):
 def closure(sources):
     closure_domain = 'closure-compiler.appspot.com'
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    raw_params = [
+    raw_params = (
         ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
         ('output_format', 'text'),
         ('output_info', 'compiled_code'),
-    ]
+    )
     filtered = []
     conn = client.HTTPConnection(closure_domain)
     for source in sources:
         print('sending {} bytes to closure api...'.format(len(source)))
-        raw_params.append(('js_code', source))
-        params = urlencode(raw_params)
+        full_params = list(raw_params) + [('js_code', source)]
+        params = urlencode(full_params)
         conn.request('POST', '/compile', params, headers)
         response = conn.getresponse()
         filtered_source_bytes = response.read()
