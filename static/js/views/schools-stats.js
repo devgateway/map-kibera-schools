30da3e36d90b75335453d64705bedd4822ec2b5f
Phil Schleihauf <uniphil@gmail.com>
2014-07-20 21:24:06 -0400

    summarize the number of schools

--- static/js/views/schools-stats.js
+++ static/js/views/schools-stats.js
@@ -1,3 +1,13 @@
 app.views.SchoolsStats = Backbone.View.extend({
 
+  template: _.template('<h3>Schools: <%= schools.length %></h3>'),
+
+  initialize: function() {
+    this.listenTo(this.collection, 'add change:excluded', this.render);
+  },
+
+  render: function() {
+    this.$el.html(this.template({schools: this.collection.notExcluded()}));
+    return this;
+  }
 });
