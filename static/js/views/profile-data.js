2cd9a1d96acd17c0ad4554c52421a4e93fcacc34
Mikel Maron <mikel_maron@yahoo.com>
2015-02-22 21:36:47 -0500

    fix jumping bug. fix fb comments box

--- static/js/views/profile-data.js
+++ static/js/views/profile-data.js
@@ -8,7 +8,7 @@ app.views.ProfileData = Backbone.View.extend({
     $('.data-selectable').removeClass('selected');
     var source = e.currentTarget.getAttribute('href').split('-').slice(-1)[0];
     $('.data-' + source).addClass('selected');
-    this.model.set('location', {osm: 0, kod:1 }[source]);
+    this.model.set('location', {osm: 0, kod:1, compare:2 }[source]);
     return false;
   }
 
